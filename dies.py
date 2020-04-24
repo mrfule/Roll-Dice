39d5684bb188985786f3b8408cfe60040e4aac20
mrfule <44532771+mrfule@users.noreply.github.com>
2020-04-24 09:56:29 +0530

    Dice

---
+++ dies.py
@@ -0,0 +1,52 @@
+import random
+
+
+x="y"
+while x== "y":
+    
+    xx = random.randint(1,6)
+    if xx ==1:
+        print("---------")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("---------")
+    
+    
+    if xx == 2:
+        print("---------")
+        print("|       |")
+        print("| 0   0 |")
+        print("|       |")
+        print("---------")
+
+    if xx == 3:
+        print("---------")
+        print("|       |")
+        print("| 0 0 0 |")
+        print("|       |")
+        print("---------")
+
+    if xx ==4:
+        print("---------")
+        print("| 0   0 |")
+        print("|       |")
+        print("| 0   0 |")
+        print("---------")
+
+    if xx ==5:
+        print("---------")
+        print("| 0   0 |")
+        print("|   0   |")
+        print("| 0   0 |")
+        print("---------")
+
+    if xx ==6:
+        print("---------")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("---------")
+
+    x=input("'y' or 'n'")
+ 
